5cb023bd2be7bda4928f95256757df84d1a190b7
Alisa Zhurova <zhurova2021@gmail.com>
2023-05-04 13:15:07 +0300

    fix: change 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const {copyFile } = require('node:fs/promises');
+const path = require('path');
 
+const folder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
+
+async function copyDir(){
+  fs.mkdir(newFolder, {recursive: true}, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Папка успешно создана');
+    }
+  });
+
+  fs.readdir(folder, (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach((file) => {
+      const filePath = path.join(__dirname, 'files', file);
+      const newFilePath = path.join(__dirname, 'files-copy', file);
+      copyFile(filePath, newFilePath);
+      console.log(file);
+    })
+  });
+}
+
+copyDir();
