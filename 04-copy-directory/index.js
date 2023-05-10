898f17e6d1e91606fd4ac50209758a8c6bab6ae3
Alisa Zhurova <zhurova2021@gmail.com>
2023-05-10 12:08:31 +0300

    fix: change  4 -copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,26 +5,27 @@ const path = require('path');
 const folder = path.join(__dirname, 'files');
 const newFolder = path.join(__dirname, 'files-copy');
 
-async function copyDir(){
+fs.rm(newFolder, {recursive: true}, (err) => {
   fs.mkdir(newFolder, {recursive: true}, (err) => {
     if (err) {
       throw err;
     } else {
       console.log('Папка успешно создана');
+      copyDir();
     }
   });
+}); 
 
-  fs.readdir(folder, (err, files) => {
+async function copyDir(){
+  fs.readdir(folder, {withFileTypes: true}, (err, files) => {
     if (err) {
       throw err;
     }
     files.forEach((file) => {
-      const filePath = path.join(__dirname, 'files', file);
-      const newFilePath = path.join(__dirname, 'files-copy', file);
+      const filePath = path.join(__dirname, 'files', file.name);
+      const newFilePath = path.join(__dirname, 'files-copy', file.name);
       copyFile(filePath, newFilePath);
       console.log(file);
     })
   });
 }
-
-copyDir();
